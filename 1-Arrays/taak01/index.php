f5fe13de400a59ae9ab68dcbfffe784a3f5ed66e
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-09 11:48:24 +0100

    taak01 af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Arman";
+$student['achternaam'] = "Kokor";
+$student['leeftijd'] = 16;
+$student['klas'] = '1D';
+
+echo "Gegevens Arman: Voornaam:". " " . " " . $student['voornaam'] . " " . "Achternaam:" . $student['achternaam'] . " " . "Leeftijd:" . " " . $student['leeftijd'] . " ". "Klas:" . " " . $student['klas'];
+?>
