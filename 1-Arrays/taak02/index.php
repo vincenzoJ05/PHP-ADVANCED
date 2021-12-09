6896f5e7a20083d8c83df10be4f5b7bd322e8805
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-09 12:13:41 +0100

    taak02 af

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,7 @@
+<?php
+$mezelf['voornaam'] = "Vincenzo";
+$mezelf['achternaam'] = "Jovanovic";
+$mezelf['leeftijd'] = 16;
+$mezelf['opleiding'] = "Software Developer.";
+
+echo "Mijn naam is" . " " . $mezelf['voornaam'] . " " . $mezelf['achternaam'] . " " . ", ik ben" . " " .  $mezelf['leeftijd'] . " " . "jaar oud". " " . "Ik zit op het ROC van amsterdam en ik doe de opleiding" . " " . $mezelf['opleiding'];
