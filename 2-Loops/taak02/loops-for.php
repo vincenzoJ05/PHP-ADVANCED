0aead0273858bec382afeae4504cdad85a4348e7
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-10 13:30:46 +0100

    taak02

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo "<ul>";
+for($loop = 0; $loop < 18; $loop++){
+     echo "<li> ik ben $loop jaar, dus ik heb geen stemrecht. </li>";
+}
+    echo "<li> Ik ben 18 jaar oud dus ik heb stemrecht. </li>";
+echo "</ul>"
+?>
+</body>
+</html>
+
