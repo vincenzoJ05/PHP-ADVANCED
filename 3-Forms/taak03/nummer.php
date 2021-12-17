c09512a1520dcf9f7c5aa0451dd603ea6e60aa2d
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-17 23:27:01 +0100

    taak03 af

---
+++ 3-Forms/taak03/nummer.php
@@ -0,0 +1,6 @@
+<?php
+foreach($_GET as $key => $value){
+    echo $value . "<br>"; 
+}
+/*http://localhost/PHP-ADVANCED/3-Forms/taak03/nummer.php?artist=michael+jackson&titel=fuck+me+in+a+tesla&year=2020&genre=rap*/
+?>
