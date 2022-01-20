b9ddb7dc5ddfae99cb0c7b01bb0b59dccd51ca1a
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-20 23:07:19 +0100

    verbeterd

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,9 +1,13 @@
  <?php
  $leeftijd = 0 ;
 
- while($leeftijd <= 18){
+ while($leeftijd < 18){
    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen" . "<br>";
    $leeftijd++;
+
+   if($leeftijd == 18){
+     echo "Ik ben $leeftijd jaar, dus ik mag stemmen";
+   }
  }
 
 
