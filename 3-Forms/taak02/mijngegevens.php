0581abfae8e7651d371acaafeeb1984293dd97ec
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-03-02 22:02:43 +0100

    html toegevoegd

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+</head>
+<body>
+
 <?php
 /*ik heb GET gebruikt omdat ik wil zien hoe het eruit ziet in de url beter is*/
 $zinnen = [
@@ -12,12 +22,16 @@ $zinnen = [
     "kleur" => "Kleur: " 
 ];
 
-
 foreach($_GET as $key => $value){
     echo $zinnen[$key].$value."<br>"; 
 }
 
 echo "<body style='background-color:".$_GET['kleur']."'>";
-?>
+?>   
+
+</body>
+</html>
+
+
 
 
